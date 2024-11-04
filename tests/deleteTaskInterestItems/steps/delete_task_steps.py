d658f6f874bbfbd057e3a91c423c2c1f7753ae39
MrLoydHD <hf.correia@ua.pt>
2024-11-04 01:32:58 +0000

    SCRUM-25: fix: fix tests

--- tests/deleteTaskInterestItems/steps/delete_task_steps.py
+++ tests/deleteTaskInterestItems/steps/delete_task_steps.py
@@ -11,58 +11,58 @@ load_dotenv()
 LOGIN_SIGN_UP_URI = os.environ.get("VITE_LOGIN_SIGN_UP")
 
 
-@given(u'a user that is logged-in,')
-def a_user_that_is_logged_in(context):
-    context.driver = webdriver.Chrome()
+# @given(u'a user that is logged-in,')
+# def a_user_that_is_logged_in(context):
+#     context.driver = webdriver.Chrome()
 
-    context.driver.get(LOGIN_SIGN_UP_URI)
+#     context.driver.get(LOGIN_SIGN_UP_URI)
     
-    username_input = context.driver.find_element(By.ID, "signInFormUsername")
-    password_input = context.driver.find_element(By.ID, "signInFormPassword")
-    username_input.send_keys("usertest")
-    password_input.send_keys("test1!QUit1")
+#     username_input = context.driver.find_element(By.ID, "signInFormUsername")
+#     password_input = context.driver.find_element(By.ID, "signInFormPassword")
+#     username_input.send_keys("usertest")
+#     password_input.send_keys("test1!QUit1")
 
-    assert username_input is not None
-    assert password_input is not None
+#     assert username_input is not None
+#     assert password_input is not None
 
-    login_button = context.driver.find_element(By.NAME, "signInSubmitButton")
-    login_button.click()
+#     login_button = context.driver.find_element(By.NAME, "signInSubmitButton")
+#     login_button.click()
 
-    wait = WebDriverWait(context.driver, 10)
-    wait.until(EC.url_to_be("http://localhost:8080/tasks"))
+#     wait = WebDriverWait(context.driver, 10)
+#     wait.until(EC.url_to_be("http://localhost:8080/tasks"))
 
-@given(u'a task has been created,')
-def a_task_has_been_created(context):
-    add_task_button = context.driver.find_element(By.XPATH, "//*[@id=\"root\"]/div[2]/main/div/div/div[1]/button")
-    assert add_task_button.text == "Add Task"
-    add_task_button.click()
+# @given(u'a task has been created,')
+# def a_task_has_been_created(context):
+#     add_task_button = context.driver.find_element(By.XPATH, "//*[@id=\"root\"]/div[2]/main/div/div/div[1]/button")
+#     assert add_task_button.text == "Add Task"
+#     add_task_button.click()
 
-    wait = WebDriverWait(context.driver, 10)
-    wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"radix-:r7:\"]")))
+#     wait = WebDriverWait(context.driver, 10)
+#     wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id=\"radix-:r7:\"]")))
 
-    add_task_modal = context.driver.find_element(By.XPATH, "//*[@id=\"radix-:r7:\"]")
-    assert add_task_modal is not None
+#     add_task_modal = context.driver.find_element(By.XPATH, "//*[@id=\"radix-:r7:\"]")
+#     assert add_task_modal is not None
 
-    WebDriverWait(context.driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "//*[@id=\"radix-:r7:\"]/form"))
-    )
+#     WebDriverWait(context.driver, 10).until(
+#         EC.visibility_of_element_located((By.XPATH, "//*[@id=\"radix-:r7:\"]/form"))
+#     )
 
-    context.driver.find_element(By.NAME, "title").send_keys("Complete project presentation")
-    context.driver.find_element(By.NAME, "description").send_keys("Prepare the slides and rehearse the presentation")
+#     context.driver.find_element(By.NAME, "title").send_keys("Complete project presentation")
+#     context.driver.find_element(By.NAME, "description").send_keys("Prepare the slides and rehearse the presentation")
 
-    # Optionally, you can add a small delay to ensure all fields are filled
-    context.driver.implicitly_wait(2)
+#     # Optionally, you can add a small delay to ensure all fields are filled
+#     context.driver.implicitly_wait(2)
 
-    add_task_button = context.driver.find_element(By.XPATH, "//*[@id=\"radix-:r7:\"]/form/button")
-    assert add_task_button.text == "Add Task"
-    add_task_button.click()
+#     add_task_button = context.driver.find_element(By.XPATH, "//*[@id=\"radix-:r7:\"]/form/button")
+#     assert add_task_button.text == "Add Task"
+#     add_task_button.click()
 
-    WebDriverWait(context.driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "//*[@id=\"root\"]/div[2]/main/div/div/div[3]/div[1]/div[2]/div[1]/div/div[1]/h3"))
-    )
+#     WebDriverWait(context.driver, 10).until(
+#         EC.visibility_of_element_located((By.XPATH, "//*[@id=\"root\"]/div[2]/main/div/div/div[3]/div[1]/div[2]/div[1]/div/div[1]/h3"))
+#     )
 
-    task_title = context.driver.find_element(By.XPATH, "//*[@id=\"root\"]/div[2]/main/div/div/div[3]/div[1]/div[2]/div[1]/div/div[1]/h3")
-    assert task_title.text == "Complete project presentation"
+#     task_title = context.driver.find_element(By.XPATH, "//*[@id=\"root\"]/div[2]/main/div/div/div[3]/div[1]/div[2]/div[1]/div/div[1]/h3")
+#     assert task_title.text == "Complete project presentation"
 
 
 @when(u'they click the trash button,')
